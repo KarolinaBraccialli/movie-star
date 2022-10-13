da2317575cbd803a42da516a5a6030ed04108c4a
Karolina Braccialli <karolinabraccialli@gmail.com>
2022-10-13 15:12:58 -0300

    Login e Logout usuário

--- models/User.php
+++ models/User.php
@@ -24,12 +24,13 @@ interface UserDAOInterface
 {
     public function buildUser($data);
     public function create(User $user, $authUser = false);
-    public function update(User $user);
+    public function update(User $user, $redirect = true);
     public function verifyToken($protected = false);
     public function setTokenToSession($token, $redirect = true);
     public function authenticateUser($email, $password);
     public function findByToken($token);
     public function findByEmail($email);
     public function findById($id);
+    public function destroyToken();
     public function chancePassword(User $user);
 }
